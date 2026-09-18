bc6c6ff15bd1b3b26b407a30f1726d1f86ea8878
gsandrade1905-droid <gs.andrade1905@gmail.com>
2026-09-18 00:42:25 -0300

    Add files via upload
    em andamento

---
+++ trabalho/src/main/java/com/mycompany/trabalho/Trabalho.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ *
+ * @author r
+ */
+public class Trabalho {
+
+    public static void main(String[] args) {
+       telaLogin tela1 = new telaLogin();
+       tela1.setVisible(true);
+    }
+}
